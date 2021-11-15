f234400c66c2bf8986f89438d6640ac0ff8c2a4e
cw0 <cwilson8706@gmail.com>
2021-11-15 16:26:51 -0500

    fix: powerlevel 10k

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ echo "Installing zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo "Installing powerlevel10k"
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo "Installing ls colors"
 # gem install colorls
