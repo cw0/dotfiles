191cf9d154503810e62fef5105a262f60b18cef7
cw0 <cwilson8706@gmail.com>
2021-11-15 14:16:47 -0500

    hopefully last typo

--- setup.sh
+++ setup.sh
@@ -42,7 +42,7 @@ fc-cache -fv
 echo "Installing nvm"
 brew install nvm
 mkdir ~/.nvm
-echo "export NVM_DIR=\"$([ -z \"${XDG_CONFIG_HOME-}\" ] && printf %s \"${HOME}/.nvm\" || printf %s \"${XDG_CONFIG_HOME}/nvm\")" >> ~/.bashrc
+echo "export NVM_DIR=$([ -z \"${XDG_CONFIG_HOME-}\" ] && printf %s \"${HOME}/.nvm\" || printf %s \"${XDG_CONFIG_HOME}/nvm\")" >> ~/.bashrc
 echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\" # This loads nvm" >> ~/.bashrc
 
 
