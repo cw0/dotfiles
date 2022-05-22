d853a811c2c6ce2c60b58f7c29002bd67ceefb90
cw0 <cwilson8706@gmail.com>
2022-05-22 01:17:26 -0400

    add aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,16 @@
+echo "Updating configs"
+rm -f ~/.p10k.zsh
+ln -sr .p10k.zsh ~/.p10k.zsh
+
+rm -f ~/.zshrc
+ln -sr .zshrc ~/.zshrc
+
+rm -f ~/.vimrc
+ln -sr .vimrc ~/.vimrc
+
+rm -f ~/.config/kitty/kitty.conf
+mkdir ~/.config/kitty
+ln -sr kitty.conf ~/.config/kitty/kitty.conf
+
+rm -f ~/.tmux.conf
+ln -sr .tmux.conf ~/.tmux.conf
