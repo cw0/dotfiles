ca2d05e38cee3079ed1c9b0155f15284fb10a3a2
cw0 <cwilson8706@gmail.com>
2023-10-09 00:19:11 -0400

    feat: configure chat gpt

--- .config/nvim/lua/plugins/chatgpt.lua
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -17,4 +17,86 @@ return {
       chatgpt.setup()
     end
   end,
+  keys = {
+    { "<leader>aa", "<cmd>ChatGPTActAs<cr>", desc = "Open Chat GPT with Awesome Prompts" },
+    {
+      "<leader>ae",
+      "<cmd>ChatGPTEditWithInstructions<cr>",
+      desc = "Chat GPT Edit With Instructions",
+      mode = { "n", "v" }
+    },
+    { "<leader>ag", "<cmd>ChatGPT<cr>",      desc = "Open Chat GPT Prompt" },
+    {
+      "<leader>arc",
+      "<cmd>ChatGPTRun complete_code<cr>",
+      desc = "Chat GPT Complete Code",
+      mode = { "n", "v" }
+    },
+    {
+      "<leader>ard",
+      "<cmd>ChatGPTRun docstring<cr>",
+      desc = "Chat GPT Docstring",
+      mode = { "n", "v" }
+    },
+    {
+      "<leader>arg",
+      "<cmd>ChatGPTRun grammar_correction<cr>",
+      desc = "Chat GPT Correct Grammar",
+      mode = { "n", "v" }
+    },
+    {
+      "<leader>arf",
+      "<cmd>ChatGPTRun fix_bugs<cr>",
+      desc = "Chat GPT Fix Bugs",
+      mode = { "n", "v" }
+    },
+    {
+      "<leader>ark",
+      "<cmd>ChatGPTRun keywords<cr>",
+      desc = "Chat GPT Keywords",
+      mode = { "n", "v" }
+    },
+    {
+      "<leader>aro",
+      "<cmd>ChatGPTRun optimize_code<cr>",
+      desc = "Chat GPT Optimize Code",
+      mode = { "n", "v" }
+    },
+    {
+      "<leader>arr",
+      "<cmd>ChatGPTRun code_readability_analysis<cr>",
+      desc = "Chat GPT Code Readability Analysis",
+      mode = { "n", "v" }
+    },
+    {
+      "<leader>arR",
+      "<cmd>ChatGPTRun roxygen_edit<cr>",
+      desc = "Chat GPT Roxygen Edit",
+      mode = { "n", "v" }
+    },
+    {
+      "<leader>ars",
+      "<cmd>ChatGPTRun summarize<cr>",
+      desc = "Chat GPT Summarize",
+      mode = { "n", "v" }
+    },
+    {
+      "<leader>art",
+      "<cmd>ChatGPTRun add_tests<cr>",
+      desc = "Chat GPT Add Tests",
+      mode = { "n", "v" }
+    },
+    {
+      "<leader>arT",
+      "<cmd>ChatGPTRun translate<cr>",
+      desc = "Chat GPT Translate",
+      mode = { "n", "v" }
+    },
+    {
+      "<leader>are",
+      "<cmd>ChatGPTRun explain_code<cr>",
+      desc = "Chat GPT Explain Code",
+      mode = { "n", "v" }
+    },
+  }
 }
