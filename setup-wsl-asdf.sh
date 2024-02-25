d3a174144469bac99838559533b70ebdf5356cee
cw0 <cwilson8706@gmail.com>
2024-02-25 14:58:07 -0500

    feat: add asdf bootstrap for wsl

---
+++ setup-wsl-asdf.sh
@@ -0,0 +1,18 @@
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add poetry https://github.com/asdf-community/asdf-poetry.git
+asdf plugin-add python
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
+asdf plugin-add terraform-ls https://github.com/asdf-community/asdf-hashicorp.git
+asdf install nodejs latest
+asdf install poetry latest
+asdf install python latest
+asdf install golang latest
+asdf install terraform latest
+asdf install terraform-ls latest
+asdf global nodejs latest
+asdf global poetry latest
+asdf global python latest
+asdf global golang latest
+asdf global terraform latest
+asdf global terraform-ls latest
