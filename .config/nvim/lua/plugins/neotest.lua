4b2f7cfb8bb3fe2da71cf521a8fc3f76d0d504ec
cw0 <chris.wilson@gmail.com>
2024-03-01 15:52:55 -0500

    fix: typescript tests

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -1,9 +1,26 @@
 return {
-  "nvim-neotest/neotest",
-  dependencies = {
+  {
     "haydenmeade/neotest-jest",
   },
-  opts = {
-    ["neotest-jest"] = {},
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "haydenmeade/neotest-jest",
+    },
+    opts = function(_, opts)
+      table.insert(
+        opts.adapters,
+        require("neotest-jest")({
+          jestCommand = "npm test --",
+          jestConfigFile = "jest.config.js",
+          cwd = function(path)
+            -- return vim.fn.getcwd()
+            local cwd = require("neotest-jest.util").find_package_json_ancestor(path)
+            return cwd
+          end,
+        })
+      )
+    end,
   },
 }
